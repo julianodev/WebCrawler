d4e0dae031ef37a657203a3c25e420ce69982d43
Lucas Juliano <lucas.juliano@live.com>
2017-09-28 21:14:43 -0300

    Crawler com .NET e paginação

--- webCrawler/frmMain.cs
+++ webCrawler/frmMain.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace webCrawler
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        public frmMain()
         {
             InitializeComponent();
         }
